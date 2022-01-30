97d18e774e1559068c92e0589e76a02e0f5c9d1e
dinastyoffreedom <info@dinastyoffreedom>
2022-01-30 01:48:41 +0100

    correzione nomi

--- assets/js/dinastycoin-gateway-order-page.js
+++ assets/js/dinastycoin-gateway-order-page.js
@@ -1,9 +1,9 @@
 /*
  * Copyright (c) 2018, Ryo Currency Project
 */
-function monero_showNotification(message, type='success') {
+function dinastycoin_showNotification(message, type='success') {
     var toast = jQuery('<div class="' + type + '"><span>' + message + '</span></div>');
-    jQuery('#monero_toast').append(toast);
+    jQuery('#dinastycoin_toast').append(toast);
     toast.animate({ "right": "12px" }, "fast");
     setInterval(function() {
         toast.animate({ "right": "-400px" }, "fast", function() {
@@ -11,95 +11,95 @@ function monero_showNotification(message, type='success') {
         });
     }, 2500)
 }
-function monero_showQR(show=true) {
-    jQuery('#monero_qr_code_container').toggle(show);
+function dinastycoin_showQR(show=true) {
+    jQuery('#dinastycoin_qr_code_container').toggle(show);
 }
-function monero_fetchDetails() {
+function dinastycoin_fetchDetails() {
     var data = {
         '_': jQuery.now(),
-        'order_id': monero_details.order_id
+        'order_id': dinastycoin_details.order_id
     };
-    jQuery.get(monero_ajax_url, data, function(response) {
+    jQuery.get(dinastycoin_ajax_url, data, function(response) {
         if (typeof response.error !== 'undefined') {
             console.log(response.error);
         } else {
-            monero_details = response;
-            monero_updateDetails();
+            dinastycoin_details = response;
+            dinastycoin_updateDetails();
         }
     });
 }
 
-function monero_updateDetails() {
+function dinastycoin_updateDetails() {
 
-    var details = monero_details;
+    var details = dinastycoin_details;
 
-    jQuery('#monero_payment_messages').children().hide();
+    jQuery('#dinastycoin_payment_messages').children().hide();
     switch(details.status) {
         case 'unpaid':
-            jQuery('.monero_payment_unpaid').show();
-            jQuery('.monero_payment_expire_time').html(details.order_expires);
+            jQuery('.dinastycoin_payment_unpaid').show();
+            jQuery('.dinastycoin_payment_expire_time').html(details.order_expires);
             break;
         case 'partial':
-            jQuery('.monero_payment_partial').show();
-            jQuery('.monero_payment_expire_time').html(details.order_expires);
+            jQuery('.dinastycoin_payment_partial').show();
+            jQuery('.dinastycoin_payment_expire_time').html(details.order_expires);
             break;
         case 'paid':
-            jQuery('.monero_payment_paid').show();
-            jQuery('.monero_confirm_time').html(details.time_to_confirm);
+            jQuery('.dinastycoin_payment_paid').show();
+            jQuery('.dinastycoin_confirm_time').html(details.time_to_confirm);
             jQuery('.button-row button').prop("disabled",true);
             break;
         case 'confirmed':
-            jQuery('.monero_payment_confirmed').show();
+            jQuery('.dinastycoin_payment_confirmed').show();
             jQuery('.button-row button').prop("disabled",true);
             break;
         case 'expired':
-            jQuery('.monero_payment_expired').show();
+            jQuery('.dinastycoin_payment_expired').show();
             jQuery('.button-row button').prop("disabled",true);
             break;
         case 'expired_partial':
-            jQuery('.monero_payment_expired_partial').show();
+            jQuery('.dinastycoin_payment_expired_partial').show();
             jQuery('.button-row button').prop("disabled",true);
             break;
     }
 
-    jQuery('#monero_exchange_rate').html('1 XMR = '+details.rate_formatted+' '+details.currency);
-    jQuery('#monero_total_amount').html(details.amount_total_formatted);
-    jQuery('#monero_total_paid').html(details.amount_paid_formatted);
-    jQuery('#monero_total_due').html(details.amount_due_formatted);
+    jQuery('#dinastycoin_exchange_rate').html('1 DCY = '+details.rate_formatted+' '+details.currency);
+    jQuery('#dinastycoin_total_amount').html(details.amount_total_formatted);
+    jQuery('#dinastycoin_total_paid').html(details.amount_paid_formatted);
+    jQuery('#dinastycoin_total_due').html(details.amount_due_formatted);
 
-    jQuery('#monero_integrated_address').html(details.integrated_address);
+    jQuery('#dinastycoin_integrated_address').html(details.integrated_address);
 
-    if(monero_show_qr) {
-        var qr = jQuery('#monero_qr_code').html('');
+    if(dinastycoin_show_qr) {
+        var qr = jQuery('#dinastycoin_qr_code').html('');
         new QRCode(qr.get(0), details.qrcode_uri);
     }
 
     if(details.txs.length) {
-        jQuery('#monero_tx_table').show();
-        jQuery('#monero_tx_none').hide();
-        jQuery('#monero_tx_table tbody').html('');
+        jQuery('#dinastycoin_tx_table').show();
+        jQuery('#dinastycoin_tx_none').hide();
+        jQuery('#dinastycoin_tx_table tbody').html('');
         for(var i=0; i < details.txs.length; i++) {
             var tx = details.txs[i];
             var height = tx.height == 0 ? 'N/A' : tx.height;
             var row = ''+
                 '<tr>'+
                 '<td style="word-break: break-all">'+
-                '<a href="'+monero_explorer_url+'/tx/'+tx.txid+'" target="_blank">'+tx.txid+'</a>'+
+                '<a href="'+dinastycoin_explorer_url+'/tx/'+tx.txid+'" target="_blank">'+tx.txid+'</a>'+
                 '</td>'+
                 '<td>'+height+'</td>'+
-                '<td>'+tx.amount_formatted+' Monero</td>'+
+                '<td>'+tx.amount_formatted+' Dinastycoin</td>'+
                 '</tr>';
 
-            jQuery('#monero_tx_table tbody').append(row);
+            jQuery('#dinastycoin_tx_table tbody').append(row);
         }
     } else {
-        jQuery('#monero_tx_table').hide();
-        jQuery('#monero_tx_none').show();
+        jQuery('#dinastycoin_tx_table').hide();
+        jQuery('#dinastycoin_tx_none').show();
     }
 
     // Show state change notifications
     var new_txs = details.txs;
-    var old_txs = monero_order_state.txs;
+    var old_txs = dinastycoin_order_state.txs;
     if(new_txs.length != old_txs.length) {
         for(var i = 0; i < new_txs.length; i++) {
             var is_new_tx = true;
@@ -110,49 +110,49 @@ function monero_updateDetails() {
                 }
             }
             if(is_new_tx) {
-                monero_showNotification('Transaction received for '+new_txs[i].amount_formatted+' Monero');
+                dinastycoin_showNotification('Transaction received for '+new_txs[i].amount_formatted+' Dinastycoin');
             }
         }
     }
 
-    if(details.status != monero_order_state.status) {
+    if(details.status != dinastycoin_order_state.status) {
         switch(details.status) {
             case 'paid':
-                monero_showNotification('Your order has been paid in full');
+                dinastycoin_showNotification('Your order has been paid in full');
                 break;
             case 'confirmed':
-                monero_showNotification('Your order has been confirmed');
+                dinastycoin_showNotification('Your order has been confirmed');
                 break;
             case 'expired':
             case 'expired_partial':
-                monero_showNotification('Your order has expired', 'error');
+                dinastycoin_showNotification('Your order has expired', 'error');
                 break;
         }
     }
 
-    monero_order_state = {
-        status: monero_details.status,
-        txs: monero_details.txs
+    dinastycoin_order_state = {
+        status: dinastycoin_details.status,
+        txs: dinastycoin_details.txs
     };
 
 }
 jQuery(document).ready(function($) {
-    if (typeof monero_details !== 'undefined') {
-        monero_order_state = {
-            status: monero_details.status,
-            txs: monero_details.txs
+    if (typeof dinastycoin_details !== 'undefined') {
+        dinastycoin_order_state = {
+            status: dinastycoin_details.status,
+            txs: dinastycoin_details.txs
         };
-        setInterval(monero_fetchDetails, 30000);
-        monero_updateDetails();
+        setInterval(dinastycoin_fetchDetails, 30000);
+        dinastycoin_updateDetails();
         new ClipboardJS('.clipboard').on('success', function(e) {
             e.clearSelection();
             if(e.trigger.disabled) return;
             switch(e.trigger.getAttribute('data-clipboard-target')) {
-                case '#monero_integrated_address':
-                    monero_showNotification('Copied destination address!');
+                case '#dinastycoin_integrated_address':
+                    dinastycoin_showNotification('Copied destination address!');
                     break;
-                case '#monero_total_due':
-                    monero_showNotification('Copied total amount due!');
+                case '#dinastycoin_total_due':
+                    dinastycoin_showNotification('Copied total amount due!');
                     break;
             }
             e.clearSelection();
